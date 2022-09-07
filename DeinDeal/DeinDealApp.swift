72676ef4e72e904c5e2caa002c7a4589a2fb01c0
mttAlexandre <alexandre.montcuit@icloud.com>
2022-09-07 20:31:47 +0200

    Initial Commit

---
+++ DeinDeal/DeinDealApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeinDealApp.swift
+//  DeinDeal
+//
+//  Created by Alexandre MONTCUIT on 07/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct DeinDealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
