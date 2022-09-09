470f7cdd9918422dc5789f2f892002685e17f618
AlexandreMontcuit <alexandre.montcuit@icloud.com>
2022-09-09 21:30:59 +0200

    Add AppDelegate for SwiftUI app (required to load sag with SDWebImage)

---
+++ DeinDeal/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  DDRAppDelegate.swift
+//  DeinDeal
+//
+//  Created by Alexandre MONTCUIT on 09/09/2022.
+//
+
+import Foundation
+import UIKit
+
+/*
+ App delegate for SwiftUI life cycle app :
+ https://www.hackingwithswift.com/quick-start/swiftui/how-to-add-an-appdelegate-to-a-swiftui-app
+ */
+final class AppDelegate: NSObject, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        return true
+    }
+}
